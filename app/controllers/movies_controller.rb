8a57984fd8960e00277d84355bad8c803fdb8ad0
Michael Abbott <michael@mjabbottdesign.com>
2021-07-19 11:06:53 -0700

    Complete user story 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
+    @actors = @movie.actors.sort_by_age
+    @avg_age = @movie.actors.average_age
   end
 end
